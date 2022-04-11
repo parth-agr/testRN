68f08738ee3b7daedd32b75b8dcd45a81c10555a
Parth Agrawal <parth@teachmint.com>
2022-04-11 10:17:32 +0530

    Upgrade teachstack package to 0.1.2
    npm install react-native-teachstack-module@latest

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, Platform, NativeEventEmitter} from 'react-native';
+import {View, Platform, NativeEventEmitter, Dimensions} from 'react-native';
 import {TeachstackModuleView} from 'react-native-teachstack-module';
 
 const App = () => {
@@ -12,7 +12,13 @@ const App = () => {
 
   return (
     <View>
-      <TeachstackModuleView url="https://video.teachmint.com/videoroom/teachmint::693479183/6194d019deddfeef8eebdc05/eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJyb29tX2lkIjoidGVhY2htaW50Ojo2OTM0NzkxODMiLCJ1aWQiOiI2MTk0ZDAxOWRlZGRmZWVmOGVlYmRjMDUiLCJ1bmFtZSI6InJhbmRvbSIsInV0eXBlIjoxLCJleHAiOjE2NDkyMTg5MDZ9.9xiC8Pd-KZmSVwkl3zydbjAUENaUoJNY4UdFNClHLlY" />
+      <TeachstackModuleView
+        url="https://video.teachmint.com/videoroom/videodemo::rn_test/test2/eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJyb29tX2lkIjoidmlkZW9kZW1vOjpybl90ZXN0IiwidWlkIjoidGVzdDIiLCJ1bmFtZSI6InRlc3QyIiwidXR5cGUiOjIsImV4cCI6MTY0OTcwMzE4OH0.OF7UqPRL3_oQAUiuEfATj6rW3j5PFREDSKilGsoC5aQ"
+        style={{
+          width: Dimensions.get('window').width,
+          height: Dimensions.get('window').height,
+        }}
+      />
     </View>
   );
 };
